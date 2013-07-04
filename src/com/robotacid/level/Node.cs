30193a6f94374dcb871e4514dc2920cdc310b9c2
tasogare66 <tasogare66@gmail.com>
2013-07-04 10:31:09 +0900

    Node.csを追加

---
+++ src/com/robotacid/level/Node.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace com.robotacid.level {
+	/**
+	 * This is a graph vertex
+	 * 
+	 * Its edges are the connections vector which we keep the same size as
+	 * the connectionsActive vector for the graph pruning stage of map building
+	 * 
+	 * @author Aaron Steed, robotacid.com
+	 */
+	public class Node{
+		
+		public int x;
+		public int y;
+		public bool visited;
+		public List<Node> connections;
+		public List<bool> connectionsActive;
+		public bool drop;
+		
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+			visited = false;
+			connections = new List<Node>();
+			connectionsActive = new List<bool>();
+			drop = false;
+		}
+		
+	}
+
+}
